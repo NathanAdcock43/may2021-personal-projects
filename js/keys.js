24f925a0aa13a2cdd8031234b3783933014dd364
Nathan Adcock <nathan.adcock43@gmail.com>
2021-05-24 19:55:19 -0500

    Feat: updated a few things

---
+++ js/keys.js
@@ -0,0 +1,12 @@
+const mapboxToken = "pk.eyJ1IjoibmF0aGFuLWFkY29jazQzIiwiYSI6ImNrbmFqbndhcjBlNXkyem1pOWZpcGg0NmcifQ.L5fHQDfRFJdhQ7THUHU14w";
+
+
+const openweatherToken = "c3107f1de6acc6217796642a4a488107";
+
+const promisesToken = "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";
+
+const APIv3Key= "AIzaSyC43dz4_7gvVFXuJYAYPuHDXnmu6p9p-Ig";
+
+const gapiClientID= "365572997645-rksbja6tjvqfd4mua82ummn0hcgdc3e7.apps.googleusercontent.com";
+
+const gapiClientSecret= "CsUYKOSwFXiLgc6C7wISAD1p";
