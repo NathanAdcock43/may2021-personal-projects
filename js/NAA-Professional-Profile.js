ee3a8f07182790f706375a578256e1d512e9f71c
Nathan Adcock <nathan.adcock43@gmail.com>
2021-08-04 17:37:25 -0500

    Profile page game toggle animated pulse; Label on game

--- js/NAA-Professional-Profile.js
+++ js/NAA-Professional-Profile.js
@@ -1,85 +1,3 @@
-// const angleRange = document.getElementById('angle-control');
-// const angleResult = document.getElementById('angle-result');
-// const root = document.querySelector('HTML');
-// const boxes = document.querySelectorAll('.box');
-//
-// angleRange.addEventListener("input", function() {
-//     const angle = angleRange.value;
-//     const angleRad = angle * Math.PI / 180;
-//     const magicNumber = Math.abs( Math.tan(angleRad) / 2 );
-//     angleResult.innerHTML =  angleRange.value + " deg";
-//     document.documentElement.style.setProperty('--angle', angleRange.value + 'deg');
-//     document.documentElement.style.setProperty('--magic-number', magicNumber);
-//
-//     // Sadly the animation only updates correctly in Firefox :-(
-//     boxes.forEach((box) => {
-//         box.style.animation = "none";
-//     });
-// }, false);
-//
-// function magnify(imgID, zoom) {
-//     var img, glass, w, h, bw;
-//     img = document.getElementById(imgID);
-//
-//     /* Create magnifier glass: */
-//     glass = document.createElement("DIV");
-//     glass.setAttribute("class", "img-magnifier-glass");
-//
-//     /* Insert magnifier glass: */
-//     img.parentElement.insertBefore(glass, img);
-//
-//     /* Set background properties for the magnifier glass: */
-//     glass.style.backgroundImage = "url('" + img.src + "')";
-//     glass.style.backgroundRepeat = "no-repeat";
-//     glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
-//     bw = 3;
-//     w = glass.offsetWidth / 2;
-//     h = glass.offsetHeight / 2;
-//
-//     /* Execute a function when someone moves the magnifier glass over the image: */
-//     glass.addEventListener("mousemove", moveMagnifier);
-//     img.addEventListener("mousemove", moveMagnifier);
-//
-//     /*and also for touch screens:*/
-//     glass.addEventListener("touchmove", moveMagnifier);
-//     img.addEventListener("touchmove", moveMagnifier);
-//     function moveMagnifier(e) {
-//         var pos, x, y;
-//         /* Prevent any other actions that may occur when moving over the image */
-//         e.preventDefault();
-//         /* Get the cursor's x and y positions: */
-//         pos = getCursorPos(e);
-//         x = pos.x;
-//         y = pos.y;
-//         /* Prevent the magnifier glass from being positioned outside the image: */
-//         if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
-//         if (x < w / zoom) {x = w / zoom;}
-//         if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-//         if (y < h / zoom) {y = h / zoom;}
-//         /* Set the position of the magnifier glass: */
-//         glass.style.left = (x - w) + "px";
-//         glass.style.top = (y - h) + "px";
-//         /* Display what the magnifier glass "sees": */
-//         glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
-//     }
-//
-//     function getCursorPos(e) {
-//         var a, x = 0, y = 0;
-//         e = e || window.event;
-//         /* Get the x and y positions of the image: */
-//         a = img.getBoundingClientRect();
-//         /* Calculate the cursor's x and y coordinates, relative to the image: */
-//         x = e.pageX - a.left;
-//         y = e.pageY - a.top;
-//         /* Consider any page scrolling: */
-//         x = x - window.pageXOffset;
-//         y = y - window.pageYOffset;
-//         return {x : x, y : y};
-//     }
-// }
-
-
-
 setInterval(setClock, 1000)
 
 const hourHand = document.querySelector('[data-hour-hand]')
@@ -102,3 +20,43 @@ function setRotation(element, rotationRatio) {
 
 setClock()
 
+
+$(document).ready(function() {
+
+    $("#gametoggle").on('click', function () {
+        var click = $(this).data('clicks');
+        if (click) {
+            console.log("clicked");
+            $('#car-2').css("visibility", "visible");
+            $('#obstacle-1').css("visibility", "visible");
+            $('#obstacle-4').css("visibility", "visible");
+            $('#lap-2').css("visibility", "visible");
+
+            $('#obstacle-2').css("visibility", "hidden");
+            $('#obstacle-3').css("visibility", "hidden");
+            $('#car-1').css("visibility", "hidden");
+            $('#lap-1').css("visibility", "hidden");
+
+            $(this).css({left: '245px'});
+
+
+        } else  {
+            console.log("clicked");
+            $('#obstacle-2').css("visibility", "visible");
+            $('#obstacle-3').css("visibility", "visible");
+            $('#car-1').css("visibility", "visible");
+            $('#lap-1').css("visibility", "visible");
+
+
+            $('#obstacle-1').css("visibility", "hidden");
+            $('#obstacle-4').css("visibility", "hidden");
+            $('#car-2').css("visibility", "hidden");
+            $('#lap-2').css("visibility", "hidden");
+
+            $(this).css({left: '215px'});
+
+        }
+        $(this).data('clicks', !click);
+    })
+
+});
